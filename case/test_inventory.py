6596fee438a74e2e41d55dc780b42a4bb3772cb6
della0506 <106648768+della0506@users.noreply.github.com>
2022-08-22 11:26:30 +0800

    Update test_inventory.py

--- case/test_inventory.py
+++ case/test_inventory.py
@@ -91,7 +91,7 @@ class InventoryTest(seldom.TestCase):
         """Open rarity select"""
         m_page.rarity_select.click()
         self.wait(1)
-        self.assertElement(m_page.rarity_list.text)
+        self.assertElement(xpath=commonFunc.pageToString(self,m_page.rarity_list))
 
     def test12(self):
         """Filter rarity-common"""
