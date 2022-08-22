d176674b0f4de2049c7c517ded8823f6048158be
della0506 <106648768+della0506@users.noreply.github.com>
2022-08-22 16:51:06 +0800

    Add test result to feishu

--- send.py
+++ send.py
@@ -2,16 +2,25 @@
 import os
 import requests
 import json
+from bs4 import BeautifulSoup
 
 def testReport():
-    """send report to Feishu"""
+    """Read test report html"""
     path = os.getcwd() + '/reports'
     name = os.listdir(path)
     print(type(name),name)
     fileName = list(filter(lambda a:a.find("result") >= 0,name))
     print(fileName)
-    #return ''.join(fileName)
+    print(path+'/'+''.join(fileName))
+    with open(path+'/'+''.join(fileName),'r') as f:
+        soup = BeautifulSoup(f.read(),'html.parser')
+        passNum = soup.select('span[class="badge badge-pill bg-soft-success text-success me-2"]')[0].text
+        failNum = soup.select('span[class="badge badge-pill bg-soft-warning text-warning me-2"]')[0].text
+        errNum = soup.select('span[class="badge badge-pill bg-soft-danger text-danger me-2"]')[0].text
+        all = passNum + failNum + errNum
+    print(all)
 
+    """Send report to Feishu"""
     p = {
     "msg_type": "interactive",
     "card": {
@@ -19,27 +28,34 @@ def testReport():
                 "wide_screen_mode": True,
                 "enable_forward": True
         },
-        "elements": [{
-                "actions": [{
-                        "tag": "button",
-                        "text": {
-                                "content": "Click For Details 🔍",
-                                "tag": "lark_md"
-                        },
-                        "url": "https://jenkins.dev-metaverse.fun/view/test/job/test-marketplace-ui-automation/Marketplace_20Test_20Report/",
-                        "type": "default",
-                        "value": {}
-                }],
-                "tag": "action"
-        }],
-        "header": {
-                "title": {
-                        "content": "🔔 Marketplace UI Automation Testing Report",
-                        "tag": "plain_text"
+        "elements": [
+            {
+                "tag": "div",
+                "text": {
+                    "content": f"{passNum}     ｜     {failNum}     ｜     {errNum}",
+                    "tag": "lark_md"
                 }
+            }, {
+                    "actions": [{
+                            "tag": "button",
+                            "text": {
+                                    "content": "🔍 CLICK HERE FOR DETAILS",
+                                    "tag": "lark_md"
+                            },
+                            "url": "https://jenkins.dev-metaverse.fun/view/test/job/test-marketplace-ui-automation/Marketplace_20Test_20Report/",
+                            "type": "default",
+                            "value": {}
+                    }],
+                    "tag": "action"
+            }],
+            "header": {
+                    "title": {
+                            "content": f"🔔 Marketplace UI Automation Test Report",
+                            "tag": "plain_text"
+                    }
+            }
         }
     }
-}
     toFS = requests.post('https://open.feishu.cn/open-apis/bot/v2/hook/1eba2ae5-9ad4-4acc-b3fe-3b53ce9f97a7',json.dumps(p))
     print(toFS.status_code, toFS.json())
     return toFS
