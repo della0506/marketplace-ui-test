6d0706ab366b14fdd9426a91e07089f609afed73
della0506 <106648768+della0506@users.noreply.github.com>
2022-08-31 15:57:50 +0800

    Create test_login.py

---
+++ case/test_login.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import seldom
+from seldom import Seldom
+from page.login_page import LoginPage
+
+class LoginTest(seldom.TestCase):
+    global page
+    page = LoginPage(Seldom.driver)
+
+    def start(self):
+        """init"""
+        page.open('https://www.delysium.com/account/login')
+        self.max_window()
+        self.assertText('Play on the Web')
+
+    def end_class(self):
+        """quit"""
+        self.close()
+
+    @classmethod
+    def setUpClass(cls):
+        cls().start()
+
+    def test01(self):
+        """login"""
+        page.play_but.click()
+        self.assertText('Forget Password')
+        page.email_input.input('wanglijie@rct.ai')
+        page.password_input.input('2wsx@WSX')
+        page.play_but.click()
+        self.wait(1)
+        self.assertText('')
