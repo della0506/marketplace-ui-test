687e5b1bce0f24986649b947bbbe0e826c2ce161
della0506 <106648768+della0506@users.noreply.github.com>
2022-08-31 15:58:33 +0800

    Create login_page.py

---
+++ page/login_page.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from poium import Page, Element
+
+class LoginPage(Page):
+    """Website Page"""
+    #play button
+    play_but = Element(xpath='//span[contains(text(),"Play")]')
+
+    #email
+    email_input = Element(xpath='//label[contains(text(),"Email")]/following-sibling::div/div/input')
+
+    #password
+    password_input = Element(xpath='//label[contains(text(),"Password")]/following-sibling::div/div/input')
